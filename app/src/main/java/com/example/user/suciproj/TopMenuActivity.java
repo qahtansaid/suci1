c090859034fc9f4f8d549c09ebfefdb7ae5f641f
qahtan <qahtansaidmartak@gmail.com>
2017-05-13 20:25:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/user/suciproj/TopMenuActivity.java
@@ -0,0 +1,24 @@
+package com.example.user.suciproj;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+public class TopMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_menu);
+
+    }
+
+    public void onClickPesan(View v){
+        Intent i = new Intent(this,OrderActivity.class);
+        startActivity(i); // Pindah Intent saat memencet CardView yang sudah diberi onClick bernama onClickPesan
+    }
+
+
+}
